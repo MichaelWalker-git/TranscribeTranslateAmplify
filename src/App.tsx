b56c09397a4c5d10faf5a6a1ad32321c061c63c1
miketran <miketran@amazon.com>
2023-04-04 09:56:47 -0700

    feat: successfully connect to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,228 @@
-import React from "react";
-import { Amplify } from "aws-amplify";
+import React, {useEffect, useState} from "react";
+import {Amplify, API, graphqlOperation, Storage} from "aws-amplify";
 import {
   AmplifyProvider,
   Authenticator,
   Button,
   Flex,
-  Image,
   Text,
   View,
 } from "@aws-amplify/ui-react";
+import {useReactMediaRecorder} from "react-media-recorder";
+import {v4 as uuid} from 'uuid';
+
 import aws_exports from "./aws-exports";
 
 import "@aws-amplify/ui-react/styles.css";
 import theme from "./theme";
-import logo from "./logo.svg";
+import {createTranslationRecordings, startTranslationSfn} from "./graphql/mutations";
+import {CreateTranslationRecordingsInput} from "./API";
+import {listTranslationRecordings} from "./graphql/queries";
+import {onUpdateTranslationRecordings} from "./graphql/subscriptions";
 
 Amplify.configure(aws_exports);
 
+const bucket = aws_exports.aws_user_files_s3_bucket || 'amplify-audiotoawss3-dev-133151-deployment';
+const region = aws_exports.aws_user_files_s3_bucket_region || 'us-east-1';
+
+interface subscriptionParams  { provider: any, value: any }
+
 const App = () => {
+  const [sourceLanguage, setSourceLanguage] = useState('en-US');
+  const [file, updateFile] = useState<File>()
+  const [recordingName, updateRecordingName] = useState('')
+  const [targetLanguage, setTargetLanguage] = useState('es-ES');
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordings, updateRecordings] = useState([])
+  const [transcription, updateTranscription] = useState('');
+  const [translation, updateTranslation] = useState('');
+  const [currMediaBlobUrl, setCurrMediaBlobUrl] = useState('');
+
+  useEffect(() => {
+    const sub =  API.graphql(
+        graphqlOperation(onUpdateTranslationRecordings)
+    );
+    // @ts-ignore
+    sub.subscribe({
+      next: (params: subscriptionParams) => {
+        if(params.value.data.onUpdateTranslationRecordings.transcription) {
+          const {transcription, translatedText, pollyLocation} = params.value.data.onUpdateTranslationRecordings;
+            if(transcription){
+              updateTranscription(transcription);
+            }
+
+          if (translatedText) {
+            updateTranslation(translatedText);
+          }
+
+          if (pollyLocation) {
+            getPollyFile(pollyLocation);
+          }
+        }
+      },
+      error: (error: any) => console.warn(error)
+    });
+  }, []);
+
+  async function getPollyFile(pollyLocation: string) {
+    const file = await Storage.get(pollyLocation, {download: true});
+    const blob = file.Body;
+    // @ts-ignore
+    const mediaBlobUrl = URL.createObjectURL(blob);
+    setCurrMediaBlobUrl(mediaBlobUrl);
+  }
+
+  async function pushRecordingToCloud(fileBlob: File) {
+    if (fileBlob) {
+      const key = `${uuid()}${recordingName}.wav`;
+
+      const recordingInput: CreateTranslationRecordingsInput = {
+        jobId: key,
+        bucket: bucket,
+        key: `raw_input/${key}`,
+        sourceLanguage: sourceLanguage,
+        targetLanguage: targetLanguage,
+      }
+
+      try {
+        await Storage.put(
+            key,
+            fileBlob,
+            {
+            contentType: 'audio/webm',
+              customPrefix: {
+                public: 'raw_input/',
+              }
+        })
+
+        const graphqlCall = await API.graphql(
+            graphqlOperation(
+                createTranslationRecordings,
+                {input: recordingInput},
+                aws_exports.aws_appsync_apiKey
+            )
+        )
+        const startStepFunction = await API.graphql(
+            graphqlOperation(
+                startTranslationSfn,
+                {input: recordingInput},
+                        aws_exports.aws_appsync_apiKey
+            )
+        );
+      } catch (err) {
+        console.log('error: ', err)
+      }
+    }
+  }
+  async function listRecordings() {
+    const recordings = await API.graphql(graphqlOperation(listTranslationRecordings));
+
+    console.log(recordings, "recordings");
+    // @ts-ignore
+    updateRecordings(recordings?.data.listRecordings.items)
+  }
+
+  const constraints = {
+    audio: true,
+    video: false,
+    onStop: (blobUrl: string, blob: Blob) => {
+      const file = new File([blob],uuid(),{ type:"audio/wav" })
+
+      pushRecordingToCloud(file);
+    }
+  };
+
+  const {status, startRecording, stopRecording, mediaBlobUrl} = useReactMediaRecorder(
+      constraints
+  );
+
+  const handleStopAction = () => {
+    setCurrMediaBlobUrl(mediaBlobUrl || '');
+    stopRecording();
+    setIsRecording(false);
+  }
+
+  interface FlagObject {
+    src: string;
+    alt: string;
+    name: string;
+  }
+
+  const outputLanguage: FlagObject[] = [
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/sv.png",
+      alt: "sv",
+      name: "Swedish",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/it.png",
+      alt: "it",
+      name: "Italian",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/ru.png",
+      alt: "ru",
+      name: "Russian",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/ja.png",
+      alt: "ja",
+      name: "Japanese",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/fr.png",
+      alt: "fr",
+      name: "French",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/ca.png",
+      alt: "ca",
+      name: "Canadian",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/es2.png",
+      alt: "es",
+      name: "Spanish",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/pl.png",
+      alt: "pl",
+      name: "Polish",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/de.png",
+      alt: "de",
+      name: "German",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/en.png",
+      alt: "en",
+      name: "English",
+    },
+    {
+      src: "https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/gb.png",
+      alt: "gb",
+      name: "British English",
+    }
+  ];
+
+  const unselectSource = () => {
+    // ...
+  };
+
+  const selectLanguage = (mode: string, language: React.SetStateAction<string>) => {
+    if (mode === 'source') {
+      setSourceLanguage(language);
+    } else {
+      setTargetLanguage(language);
+    }
+  };
+  const handleClickableImage = (source: string, language: string) => {
+    unselectSource();
+    selectLanguage(source, language);
+  }
+
+
   return (
     <AmplifyProvider theme={theme}>
       <Authenticator>
@@ -31,10 +236,6 @@ const App = () => {
             gap="1rem"
             textAlign="center"
           >
-            <View width="100%">
-              <Image src={logo} alt="logo" width={240} height={240} />
-            </View>
-
             {user && (
               <View width="100%">
                 <Text>Hello {user.username}</Text>
@@ -45,9 +246,68 @@ const App = () => {
             )}
 
             <View width="100%">
-              <Text>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </Text>
+              <div>
+                <img src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/logo.png"
+                     alt={"logo"}
+                     height="80"/>
+              </div>
+              <div className={`languageSelectionWrapper`}>
+                Select Source Language
+                <div className={`flagRow`}>
+                  <div className="flags">
+                    <img id="source_en"
+                         className='clickableImage'
+                         src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/en2.png"
+                         height="100"
+                         onClick={() => handleClickableImage('source', 'en-US')}/>
+                    <br/>US <br/>English
+                  </div>
+                  <div className="flags">
+                    <img id="source_es"
+                         alt={"es"}
+                         className='clickableImage'
+                         src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/es.png"
+                         height="100"
+                         onClick={() => handleClickableImage('source', 'es-ES')}/>
+                    <br/>US <br/>Spanish
+                  </div>
+
+                </div>
+              </div>
+
+
+              <div className={`languageSelectionWrapper`}>
+                Select Target Language
+                <div className={`flagRow`}>
+                  {outputLanguage.map((flag: FlagObject) => (
+                      <div className="flags"
+                           key={flag.alt}>
+                        <img id={`target_${flag.alt}`}
+                             className='clickableImage'
+                             src={flag.src}
+                             height="100"
+                             alt={flag.alt}
+                             onClick={() => handleClickableImage('target', flag.alt)}/>
+                        <br/>{flag.name}<br/>&nbsp;
+                      </div>
+                  ))}
+                </div>
+              </div>
+              {
+                recordings.length >  0 && recordings.map((p, i) => (
+                    <video
+                        // style={styles.image}
+                        key={i}
+                        src={p}
+                    />
+                ))
+              }
+              <p>{transcription.length > 0 && transcription}</p>
+              <p>{translation.length > 0 && translation}</p>
+              <p>{status}</p>
+              <button onClick={startRecording}>Start Recording</button>
+              <button onClick={handleStopAction}>Stop Recording</button>
+              <video src={currMediaBlobUrl} controls autoPlay/>
             </View>
           </Flex>
         )}
